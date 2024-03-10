efd1ba1f8999e515481e411c0072df4a908e9209
seckin <skuvandik@gmail.com>
2024-03-10 04:01:11 +0300

    Implement chat function

--- p4_client.py
+++ p4_client.py
@@ -6,15 +6,30 @@
 # Copied from /OR/ Adapted from /OR/ Based on:
 # Textbook: Computer Networking: A Top-Down Approach 8th Edition, James F. Kurose, Keith W. Ross
 # Source URL: https://docs.python.org/3/howto/sockets.html#using-a-socket
+# Source URL: https://realpython.com/python-sockets/
 
 from socket import *
 
 server_name = "127.0.0.1"
 server_port = 12000
+size_limit = 4096  # All messages sent must be limited to 4096 bytes
 client_socket = socket(AF_INET, SOCK_STREAM)
 client_socket.connect((server_name, server_port))
-sentence = input("Input lowercase sentence: ")
-client_socket.send(sentence.encode())
-modifiedSentence = client_socket.recv(2048)
-print("From Server: ", modifiedSentence.decode())
+
+while True:
+    message = input("Enter Input > ")
+    client_socket.send(message.encode())
+
+    if message == "/q":
+        print("Quitting...")
+        break
+
+    response = client_socket.recv(size_limit).decode()
+    if response == "/q":
+        print("Server has requested to quit.")
+        break
+    else:
+        print(f"From Server: {response}")
+
 client_socket.close()
+print("Connection closed.")
--- p4_server.py
+++ p4_server.py
@@ -6,22 +6,35 @@
 # Copied from /OR/ Adapted from /OR/ Based on:
 # Textbook: Computer Networking: A Top-Down Approach 8th Edition, James F. Kurose, Keith W. Ross
 # Source URL: https://docs.python.org/3/howto/sockets.html#using-a-socket
+# Source URL: https://realpython.com/python-sockets/
 
 from socket import *
 
 server_port = 12000
+size_limit = 4096  # All messages sent must be limited to 4096 bytes
 server_socket = socket(AF_INET, SOCK_STREAM)
+server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 server_socket.bind(("", server_port))
 server_socket.listen(1)
 print("The server is ready to receive")
 
 while True:
     connection_socket, addr = server_socket.accept()
+    print(f"Connection established with {addr}")
     # When a client knocks on this door, the program invokes the accept() method for
     # server_socket, which creates a new socket in the server, called connection_socket,
     # dedicated to this particular client.
 
-    sentence = connection_socket.recv(1024).decode()
-    capitalizedSentence = sentence.upper()
-    connection_socket.send(capitalizedSentence.encode())
+    while True:
+        message = connection_socket.recv(size_limit).decode()
+        if message == "/q":
+            print("Client has requested to quit.")
+            connection_socket.send("/q".encode())
+            break
+        else:
+            print(f"From Client: {message}")
+            response = input("Enter Response > ")
+            connection_socket.send(response.encode())
+
     connection_socket.close()
+    print("Connection closed.")
